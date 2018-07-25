320a9e871c7ec831720c57b435988b6e11c61b15
Dinesh Babu <dineshbabu_kp@yahoo.com>
2018-07-25 19:52:22 +0100

    Hello world app

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<html>Hello World</html>'
+})
+export class EventsAppComponent {
+  title = 'app';
+}
